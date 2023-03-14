2b8ae95bfc3249ee6fab4a5928d93b18533fbaa3
cyborg <pedrozonta6@gmail.com>
2023-03-14 09:42:21 -0300

    criando classe do mysql e criando gitignore.

---
+++ CrudSimplesContainer/Connection/ConexaoMySql.cs
@@ -0,0 +1,15 @@
+using System.Data;
+using CrudSimplesContainer.Connection.Interfaces;
+using MySql.Data.MySqlClient;
+
+namespace CrudSimplesContainer.Connection;
+
+public class ConexaoMySql : IFabricaDeConexao
+{
+    private readonly IConfiguration _configuration;
+
+    public ConexaoMySql(IConfiguration configuration) => _configuration = configuration;
+
+    public IDbConnection RetornarNovaConexao() 
+        => new MySqlConnection(_configuration.GetConnectionString("Mysql"));
+}
